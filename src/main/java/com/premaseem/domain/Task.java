bfd3de6cabe51177293e09bcc8b5db10eca1b721
Aseem Jain <premaseem.webmaster@gmail.com>
2018-09-19 15:24:31 -0500

    added the entity task in project

---
+++ src/main/java/com/premaseem/domain/Task.java
@@ -0,0 +1,112 @@
+package com.premaseem.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A Task.
+ */
+@Entity
+@Table(name = "task")
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "completed")
+    private Boolean completed;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Task name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public Task dueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+        return this;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Boolean isCompleted() {
+        return completed;
+    }
+
+    public Task completed(Boolean completed) {
+        this.completed = completed;
+        return this;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        if (task.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), task.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", dueDate='" + getDueDate() + "'" +
+            ", completed='" + isCompleted() + "'" +
+            "}";
+    }
+}
